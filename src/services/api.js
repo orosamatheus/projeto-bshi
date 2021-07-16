c36cb206a2cc69e33a7dcc7717e459a4d0836078
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-16 08:53:28 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({ baseURL: 'https://simpleanime.p.rapidapi.com/',  headers: {
-    'x-rapidapi-key': '52e9263e28mshf17bad88e09c3dcp18a479jsn8d5b61860004',
+    'x-rapidapi-key': process.env.REACT_APP_API_KEY,
     'x-rapidapi-host': 'simpleanime.p.rapidapi.com'
   } });
 
