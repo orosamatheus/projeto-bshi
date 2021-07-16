fd6e2a4887913d5107cc18d96d13fb0fc47963cf
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-16 12:31:16 -0300

    feat: o que fazer por aqui

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import "./styles.scss"
 import api from '../../services/api'
+
 import arrow from "../../assets/arrow.svg"
 import logoBackground from "../../assets/logoBackground.svg"
+import dragon from "../../assets/dragon.svg"
+import fire from "../../assets/fire.svg"
+import infinite from "../../assets/infinite.svg"
 
 
 export default function Home() {
@@ -15,10 +19,10 @@ export default function Home() {
   console.log(anime)
 
   return (
-    <>
+    <div className="contentContainer">
       <div className="backgroundContainer">
         <div className="lineContainer">
-          <img alt='logo' src={logoBackground} />
+          <img className='logoBackground' alt='logo' src={logoBackground} />
           <p>
             monitore os seus animes<br />
             preferidos, e nao perca mais<br />
@@ -27,7 +31,42 @@ export default function Home() {
           <img className='arrow' alt='arrow' src={arrow} />
         </div>
       </div>
-      <h1>o que fazer por aqui</h1>
-    </>
+      <h1 className='whatYouDo'>o que fazer por aqui?</h1>
+      <div className="options">
+        <div className='option1'>
+          <img alt='icon' src={fire}/>
+          <p>
+          <span>Os Melhores</span><br/>
+          Veja o que esta<br/> 
+          pegando fogo, no<br/> 
+          mundo dos animes.<br/> 
+          Os animes mais<br/> 
+          bem avaliados pela<br/> 
+          galera!
+          </p>
+        </div>
+        <div className='option2'>
+          <img alt='icon' src={infinite}/>
+          <p>
+          <span>Informações</span><br/>
+          Procure informações<br/>
+          dos seus animes<br/>  
+          preferidos.
+          </p>
+        </div>
+        <div className='option3'>
+          <img alt='icon' src={dragon}/>
+          <p>
+          <span>Sua Lista</span><br/>
+          Comece agora mesmo<br/>
+          a montar sua lista de<br/> 
+          animes preferidos, e<br/>
+          seja notificado<br/>
+          quando um episódio<br/>
+          novo for ao ar!
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
