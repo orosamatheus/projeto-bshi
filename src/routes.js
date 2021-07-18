3cc71196a519e73ed1493958cb6894d9a4c05759
Matheus Rosa <matheusrosa.la@gmail.com>
2021-07-18 18:52:58 -0300

    deploy

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import {
     Switch,
     Route,
-    BrowserRouter as Router
+    BrowserRouter as Router,
+    Redirect
 } from 'react-router-dom';
 
 
@@ -15,8 +16,9 @@ export function Routes() {
     return (
         <Router>
             <Switch>
-                <Route exact path="https://orosamatheus.github.io/projeto-bshi/" component={Home} />
-                <Route path={`https://orosamatheus.github.io/projeto-bshi/details/:id`} component={Details} />
+                <Redirect exact from="/" to ="/home"/>
+                <Route exact path="/home" component={Home} />
+                <Route path={`/details/:id`} component={Details} />
             </Switch>
         </Router>
     )
